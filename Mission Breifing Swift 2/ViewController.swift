340690c47191382da91a52822a027871a3255c02
Allen Spicer <allenspicer@gmail.com>
2016-05-11 08:51:57 -0400

    Altered autolayout & Consolidated Code

--- Mission Breifing Swift 2/ViewController.swift
+++ Mission Breifing Swift 2/ViewController.swift
@@ -7,44 +7,13 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
     @IBOutlet weak var 🙊: UITextField!
     @IBOutlet weak var 🐔: UITextView!
     @IBOutlet weak var 🌂: UITextField!
-    @IBOutlet weak var 🐠: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        🐠.text = ""
-        🌂.text = ""
-        🐔.text = ""
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    @IBOutlet weak var 🐠: UILabel!;override func viewDidLoad() {super.viewDidLoad();🐠.text = "";🌂.text = "";🐔.text = ""};override func didReceiveMemoryWarning() {super.didReceiveMemoryWarning()}
     @IBAction func 🐝(sender: AnyObject) {
-        if (🙊.isFirstResponder()){
-            🙊.resignFirstResponder()
-        }
-    
-        let agentNameArray = 🙊.text?.componentsSeparatedByString(" ")
-        
-        if (agentNameArray!.last!.isEmpty){
-            self.view.backgroundColor = .redColor()
-            🐔.text = "MISSION COMPROMISED"
-            🐠.text = "INVALID"
-        }else{
-            self.view.backgroundColor = .greenColor()
-            
-            let lastName = agentNameArray!.last
-            
-            🐠.text = "Good evening, Agent \(lastName!)"
-            🐔.textColor = .lightGrayColor()
-            🐔.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName!), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
+        if (🙊.isFirstResponder()){🙊.resignFirstResponder()};let agentNameArray = 🙊.text?.componentsSeparatedByString(" ");if (agentNameArray!.last!.isEmpty){self.view.backgroundColor = .redColor();🐔.text = "MISSION COMPROMISED";🐠.text = "INVALID"}else{self.view.backgroundColor = .greenColor();let lastName = agentNameArray!.last;🐠.text = "Good evening, Agent \(lastName!)";🐔.textColor = .lightGrayColor();🐔.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName!), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
         }
     }
     
